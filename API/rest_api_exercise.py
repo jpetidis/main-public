7e06eda5df767298700d20c1b91732729972a43f
jpetidis <33852961+jpetidis@users.noreply.github.com>
2020-10-30 11:29:46 +1100

    Add functions

--- API/rest_api_exercise.py
+++ API/rest_api_exercise.py
@@ -10,6 +10,8 @@ Requirements - Strong understanding of JSON and python functions.
 - To complete exercise the output will need to show the price, high, low and 
 volume of a valid crypto currency on the TradeOgre Exchange 
 
+- Optional - use ticker and order book functions to retrieve crypto currency data
+
 - Current output is: ""Cannot find data for specified crypto, please try again"" 
 """
 
@@ -18,49 +20,64 @@ volume of a valid crypto currency on the TradeOgre Exchange
 #####################################################################################################################
 
 # Get ALL market data from exchange (API call)
-def get_markets():
+def get_market_data():
     url = "https://tradeogre.com/api/v1/markets"
     response = requests.get(url, verify=False)
     market_prices = json.loads(response.text)
     return market_prices
 
 
-# Output ALL market data prices
-def output_market_prices(market_data):
-    print(f"Market Prices Are {market_data}")
+# Print ALL market data from TradeOgre API
+def print_all_market_data(market_data):
+    print(f"Market Prices Are: {market_data}")
 
 
-# Specify crypto currency (Nested Dictionary from API call) to retrieve price, high, low and volume (key/pairs)
+# Specify crypto currency (Nested Dictionary from API call) to retrieve price, high, low and volume (key/values)
 def get_crypto_currency_data(market_prices, crypto_currency=None):
-
-    # Explain from line 36 to 43
     for item in market_prices:
         if crypto_currency in item:
-            dict_layout = {'price': item[crypto_currency]['price'], 'high': item[crypto_currency]['high'],
-                           'low': item[crypto_currency]['low'], 'volume': item[crypto_currency]['volume']}
-            return f"\n{crypto_currency} Price: {dict_layout['price']}\n{crypto_currency} " \
-                   f"Volume: {dict_layout['volume']}" \
-                   f"\n{crypto_currency} High: {dict_layout['high']}\n{crypto_currency} Low: {dict_layout['low']}"
+            output = f"\n{crypto_currency} Price: {item[crypto_currency]['price']}\n{crypto_currency} "\
+                     f"Volume: {item[crypto_currency]['volume']}"\
+                     f"\n{crypto_currency} High: {item[crypto_currency]['high']} " \
+                     f"\n{crypto_currency} Low: {item[crypto_currency]['low']}"
+            print(output)
+            return
+
+    print("Cannot find data for specified crypto, please try again")
 
 
-def output_crypto_currency_data(crypto_data):
-    if crypto_data:
-        print(crypto_data)
-    else:
-        print("Cannot find data for specified crypto, please try again")
+# Optional Functions
+# ###################################################################################################################
+
+# Retrieve the ticker for specified crypto currency, volume, high, and low are in the last 24 hours, initialprice is
+# the price from 24 hours ago
+def get_crypto_currency_ticker(crypto_currency=None):
+    url = f"https://tradeogre.com/api/v1/ticker/{crypto_currency}"
+    response = requests.get(url, verify=False)
+    ticker = json.loads(response.text)
+    print(f"{crypto_currency}: {ticker}")
+
+
+# Retrieve the current order book for specified crypto currency
+def get_crypto_currency_order_book(crypto_currency=None):
+    url = f"https://tradeogre.com/api/v1/orders/{crypto_currency}"
+    response = requests.get(url, verify=False)
+    order_book = json.loads(response.text)
+    print(f"{crypto_currency}: {order_book}")
 
 
 #####################################################################################################################
 
 def main():
     # Step 1 - Get market prices from exchange
-    market_data = get_markets()
+    market_data = get_market_data()
 
-    # Step 2 - Specify nested dictionary from market_data to retrieve crypto data
-    crypto_currency_data = get_crypto_currency_data(market_data)
+    # Step 2 - Specify nested dictionary from market_data and output result
+    get_crypto_currency_data(market_data, "LTC-TRTL")
 
-    # Step 3 - Print out to user
-    output_crypto_currency_data(crypto_currency_data)
+    # Optional - use ticker and order book functions
+    # get_crypto_currency_ticker()
+    # get_crypto_currency_order_book()
 
 
 #####################################################################################################################
