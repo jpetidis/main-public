8bc95a8c8477d6e10cfba661be1aa69167ab62b8
jpetidis <33852961+jpetidis@users.noreply.github.com>
2021-02-22 16:47:04 +1100

    Create aws_describe_subnets.py

---
+++ API/aws_describe_subnets.py
@@ -0,0 +1,34 @@
+import boto3
+
+def describe_subnets(vpc_id):
+    response = ec2.describe_subnets(
+        Filters=[
+            {
+                'Name': "vpc-id",
+                "Values": ["vpc-32bed556"]
+            }
+        ]
+    )
+    return response
+
+
+
+# Initiate boto3 session
+session = boto3.session.Session(profile_name='saml')
+ec2 = session.client('ec2', region_name="ap-southeast-2")
+vpc_info = []
+
+
+vpc_id = ""
+response = describe_subnets(vpc_id)
+
+
+for item in response['Subnets']:
+    subnetId = item['SubnetId']
+    for tag in item['Tags']:
+        if tag['Key'] == 'Name':
+            name = tag['Value']
+            dict_layout = {'name': name, 'subnetId': subnetId}
+            vpc_info.append(dict_layout)
+
+print(vpc_info)
