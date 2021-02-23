c19516903ab55c4442db265a84d280c340bbb0fe
James Petidis <33852961+jpetidis@users.noreply.github.com>
2021-02-23 14:06:10 +1100

    add aws cred

--- API/aws_describe_subnets.py
+++ API/aws_describe_subnets.py
@@ -12,14 +12,28 @@ def describe_subnets(vpc_id):
     )
     return response
 
+# Enter AWS credentials if not using saml
+aws_access_key_id = ""
+aws_secret_access_key = ""
+aws_session_token = ""
+
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    aws_session_token=aws_session_token
+)
+
+# OR if using saml:
+#session = boto3.session.Session(profile_name='saml')
+
 
 # Initiate boto3 session
-session = boto3.session.Session(profile_name='saml')
 ec2 = session.client('ec2', region_name="ap-southeast-2")
 vpc_info = []
 
+
 # Enter VPC ID to show all subnets
-vpc_id = ""
+vpc_id = "vpc-32bed556"
 response = describe_subnets(vpc_id)
 
 for item in response['Subnets']:
